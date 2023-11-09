363c8533365dfb2c0cd51c613c81636a4c6e78bc
Rommel Silva <kamakayasilva@gmail.com>
2023-11-09 15:25:13 -0700

    added necessary configs for Caddy server deployment

--- rxconfig.py
+++ rxconfig.py
@@ -1,6 +1,13 @@
 import reflex as rx
+import os
+
+railway_domain = "RAILWAY_PUBLIC_DOMAIN"
 
 config = rx.Config(
     app_name="personal_website",
-    tailwind={}
+    telemetry_enabled=False,
+    frontend_port=3000, # default frontend port
+    backend_port=8000, # default backend port
+    # use https and the railway public domain with a backend route if available, otherwise default to a local address
+    api_url=f'https://{os.environ[railway_domain]}/backend' if railway_domain in os.environ else "http://127.0.0.1:8000"
 )
